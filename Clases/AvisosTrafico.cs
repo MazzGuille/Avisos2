7c68a009635632565266cddf186d9316027fd187
MazzGuille <mazzcode@gmail.com>
2022-05-24 16:04:44 -0300

    First stash commit

---
+++ Clases/AvisosTrafico.cs
@@ -0,0 +1,40 @@
+﻿namespace Avisos.Clases
+{
+    class AvisosTrafico : IAvisos
+    {
+        public AvisosTrafico()
+        {
+            remitente = "DGT";
+
+            mensaje = "Sancion cometida. Pague antes de 3 dias para recibir un descuento\n";
+
+            fecha = "";
+        }
+
+        public AvisosTrafico(string remitente, string mensaje, string fecha)
+        { 
+
+            this.remitente = remitente;
+            this.mensaje = mensaje;
+            this.fecha = fecha;
+
+        }
+
+        public string GetFecha()
+        {
+            return fecha;
+        }
+
+        public void MostarAviso()
+        {
+            WriteLine("Mensaje {0} ha sido enviado por {1} el dia {2}", mensaje, remitente, fecha);
+        }
+
+
+        private string remitente;
+
+        private string mensaje;
+
+        private string fecha;
+    }
+}
---
+++ GlobalUsings.cs
@@ -0,0 +1,3 @@
+global using static System.Console;
+global using Avisos.Inter;
+global using Avisos.Clases;
